aa9c56bbba5afacb84948f2843fe9e8bb61b5a6f
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-09-30 23:46:37 -0600

    feat(NewProduct): finish styles and HTML code for the form

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,23 +1,53 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 const NewProduct = () => {
+
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [price, setPrice] = useState("");
+
+    const createProduct = () => {
+
+    }
+
     return (
         <Layout>
-            <h1 className="font-light">New Product</h1>
-            <div>
-                <label htmlFor="prodName" >Product Name</label>
-                <input type="text" placeholder="Laptop..." id="prodName" className="" />
-            </div>
-            <div>
-                <label htmlFor="prodPrice" >Price ($USD)</label>
-                <input type="number" placeholder="54.99" id="prodPrice" />
-            </div>
-            <div>
-                <label htmlFor="prodDesc" >Description</label>
-                <textarea placeholder="It's a product with..." id="prodDesc"></textarea>
-            </div>
+            <form onSubmit={createProduct}>
+                <h1 className="font-light">New Product</h1>
+                <div>
+                    <label htmlFor="prodName" >Product Name</label>
+                    <input 
+                        type="text" 
+                        placeholder="Laptop..." 
+                        id="prodName" 
+                        value={title} 
+                        onChange={eventInp => setTitle(eventInp.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="prodPrice" >Price ($USD)</label>
+                    <input 
+                        type="number" 
+                        placeholder="54.99" 
+                        id="prodPrice"
+                        value={price}
+                        onChange={eventInp => setPrice(eventInp.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="prodDesc" >Description</label>
+                    <textarea 
+                        placeholder="It's a product with..." 
+                        id="prodDesc" 
+                        value={desc}
+                        onChange={eventText => setDesc(eventText.target.value)} />
+                </div>
 
-            <button className="btn-primary">Save</button>
+                <button 
+                    type="submit"
+                    className="btn-primary">
+                        Save
+                </button>
+            </form>
         </Layout>
     )
 }
