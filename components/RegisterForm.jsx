0fea31815051969ec09ee187f91aea347d0c7a6e
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-10-02 22:01:42 -0600

    feat(RegisterPage): set states for the fields of the Register form

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,12 +1,30 @@
+"use-client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 const RegisterForm = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //If click on the submit button, the page doesn't reload
+
+        if(!name || !email || !password) {
+            setError("All fields are necessary");
+            return;
+        }
+    }
+
     return (
         <main className="grid place-items-center h-screen">
             <div className="shadow-lg p-5 rounded-lg border-t-4 border-indigo-400">
                 <h1 className="text-xl font-bold my-4">Register</h1>
 
-                <form className="flex flex-col gap-2">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-2">
 
                     <div className="flex flex-col">
                         <label htmlFor="userName" >Full Name</label>
@@ -15,8 +33,8 @@ const RegisterForm = () => {
                             placeholder="Stephen Cruise" 
                             id="userName"
                             className="inputLogin"
-                            // value={title} 
-                            // onChange={eventInp => setTitle(eventInp.target.value)} 
+                            value={name} 
+                            onChange={eventInp => setName(eventInp.target.value)} 
                         />
                     </div>
 
@@ -27,8 +45,8 @@ const RegisterForm = () => {
                             placeholder="iamalbert@gmail.com" 
                             id="userEmail"
                             className="inputLogin"
-                            // value={title} 
-                            // onChange={eventInp => setTitle(eventInp.target.value)} 
+                            value={email} 
+                            onChange={eventInp => setEmail(eventInp.target.value)} 
                         />
                     </div>
 
@@ -39,14 +57,17 @@ const RegisterForm = () => {
                             placeholder="********" 
                             id="userPassword"
                             className="inputLogin" 
-                            // value={title} 
-                            // onChange={eventInp => setTitle(eventInp.target.value)} 
+                            value={password} 
+                            onChange={eventInp => setPassword(eventInp.target.value)} 
                         />
                     </div>
 
                     <button className="btn-primary">Register</button>
 
-                    <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">Error message</div>
+                    {error && (
+                        /* This code will run only if we have any error */
+                        <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">{error}</div>
+                    )}
                     
                     <Link href={"/"} className="text-sm mt-3 text-right">
                         Already have an acount? <span className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-[length:100%_3px] bg-no-repeat bg-bottom">Login</span>
